54714cbefc47d73968ea4b719d7396c4cb49bf69
Лидия Платицына <ms.lida00@mail.ru>
2021-12-13 19:15:03 +0600

    Переключение между экранами приложения

---
+++ ToAnotherScreen/app/src/main/java/com/example/toanotherscreen/SecondActivity.kt
@@ -0,0 +1,22 @@
+package com.example.toanotherscreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import android.widget.TextView
+
+
+
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        val user = "ЖЫвотное"
+        val gift = "дырку от бублика"
+
+        val infoTextView = findViewById<TextView>(R.id.textView)
+        infoTextView.text = "$user , вам передали $gift"
+    }
+}
